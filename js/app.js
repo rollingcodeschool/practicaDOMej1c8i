f6d00675fc40bc024151521094a60a6d01c50aa5
Emilse Arias <emilsearias@gmail.com>
2022-08-10 17:58:49 -0300

    agregamos la funcion adivinar numero

---
+++ js/app.js
@@ -0,0 +1,20 @@
+let botonComenzar = document.querySelector('#btnComenzar');
+let formulario = document.querySelector('#formJuego');
+let numeroMagico = 0;
+//agregar eventos desde javascript
+// botonComenzar.addEventListener('click', ()=>{ comenzarJuego(variable)});
+botonComenzar.addEventListener('click', comenzarJuego);
+formulario.addEventListener('submit', adivinarNumero);
+
+function comenzarJuego(){
+  numeroMagico = Math.floor(Math.random() * (10 + 1 -1) + 1) ;
+ console.log(numeroMagico);
+}
+
+function adivinarNumero(e){
+  e.preventDefault();
+  console.log('desde la funcion adivinar numero');
+  let numeroIngresado = document.querySelector('input').value
+  console.log(numeroIngresado)
+  //completar las preguntas correspondientes
+}
